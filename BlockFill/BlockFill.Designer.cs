e6379c4593ccc9a5e3872d271c464b0adc52e231
Ka Chun Yung <kcyung780@hotmail.com>
2018-01-17 17:18:04 -0700

    Add project files.

---
+++ BlockFill/BlockFill.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BlockFill
+{
+    partial class BlockFill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_BTN_Start = new System.Windows.Forms.Button();
+            this.UI_TB_Steps = new System.Windows.Forms.TextBox();
+            this.UI_LBL_Steps = new System.Windows.Forms.Label();
+            this.UI_RB_Unbiased = new System.Windows.Forms.RadioButton();
+            this.UI_RB_Biased = new System.Windows.Forms.RadioButton();
+            this.UI_GB_Path = new System.Windows.Forms.GroupBox();
+            this.UI_GB_Path.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UI_BTN_Start
+            // 
+            this.UI_BTN_Start.Location = new System.Drawing.Point(81, 130);
+            this.UI_BTN_Start.Name = "UI_BTN_Start";
+            this.UI_BTN_Start.Size = new System.Drawing.Size(75, 23);
+            this.UI_BTN_Start.TabIndex = 0;
+            this.UI_BTN_Start.Text = "Start";
+            this.UI_BTN_Start.UseVisualStyleBackColor = true;
+            this.UI_BTN_Start.Click += new System.EventHandler(this.UI_BTN_Start_Click);
+            // 
+            // UI_TB_Steps
+            // 
+            this.UI_TB_Steps.Location = new System.Drawing.Point(81, 91);
+            this.UI_TB_Steps.Name = "UI_TB_Steps";
+            this.UI_TB_Steps.Size = new System.Drawing.Size(75, 20);
+            this.UI_TB_Steps.TabIndex = 1;
+            this.UI_TB_Steps.Text = "0";
+            this.UI_TB_Steps.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // UI_LBL_Steps
+            // 
+            this.UI_LBL_Steps.AutoSize = true;
+            this.UI_LBL_Steps.Location = new System.Drawing.Point(38, 98);
+            this.UI_LBL_Steps.Name = "UI_LBL_Steps";
+            this.UI_LBL_Steps.Size = new System.Drawing.Size(37, 13);
+            this.UI_LBL_Steps.TabIndex = 2;
+            this.UI_LBL_Steps.Text = "Steps:";
+            // 
+            // UI_RB_Unbiased
+            // 
+            this.UI_RB_Unbiased.AutoSize = true;
+            this.UI_RB_Unbiased.Checked = true;
+            this.UI_RB_Unbiased.Location = new System.Drawing.Point(20, 19);
+            this.UI_RB_Unbiased.Name = "UI_RB_Unbiased";
+            this.UI_RB_Unbiased.Size = new System.Drawing.Size(110, 17);
+            this.UI_RB_Unbiased.TabIndex = 3;
+            this.UI_RB_Unbiased.TabStop = true;
+            this.UI_RB_Unbiased.Text = "Random Direction";
+            this.UI_RB_Unbiased.UseVisualStyleBackColor = true;
+            // 
+            // UI_RB_Biased
+            // 
+            this.UI_RB_Biased.AutoSize = true;
+            this.UI_RB_Biased.Location = new System.Drawing.Point(20, 39);
+            this.UI_RB_Biased.Name = "UI_RB_Biased";
+            this.UI_RB_Biased.Size = new System.Drawing.Size(102, 17);
+            this.UI_RB_Biased.TabIndex = 4;
+            this.UI_RB_Biased.Text = "Biased Direction";
+            this.UI_RB_Biased.UseVisualStyleBackColor = true;
+            // 
+            // UI_GB_Path
+            // 
+            this.UI_GB_Path.Controls.Add(this.UI_RB_Biased);
+            this.UI_GB_Path.Controls.Add(this.UI_RB_Unbiased);
+            this.UI_GB_Path.Location = new System.Drawing.Point(34, 12);
+            this.UI_GB_Path.Name = "UI_GB_Path";
+            this.UI_GB_Path.Size = new System.Drawing.Size(160, 73);
+            this.UI_GB_Path.TabIndex = 6;
+            this.UI_GB_Path.TabStop = false;
+            this.UI_GB_Path.Text = "Movement";
+            // 
+            // BlockFill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 159);
+            this.Controls.Add(this.UI_GB_Path);
+            this.Controls.Add(this.UI_LBL_Steps);
+            this.Controls.Add(this.UI_TB_Steps);
+            this.Controls.Add(this.UI_BTN_Start);
+            this.Name = "BlockFill";
+            this.Text = "Block Fill";
+            this.UI_GB_Path.ResumeLayout(false);
+            this.UI_GB_Path.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UI_BTN_Start;
+        private System.Windows.Forms.TextBox UI_TB_Steps;
+        private System.Windows.Forms.Label UI_LBL_Steps;
+        private System.Windows.Forms.RadioButton UI_RB_Unbiased;
+        private System.Windows.Forms.RadioButton UI_RB_Biased;
+        private System.Windows.Forms.GroupBox UI_GB_Path;
+    }
+}
+
---
+++ BlockFill/BlockFill.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using System.Threading;
+
+namespace BlockFill
+{
+    public partial class BlockFill : Form
+    {
+        // Delegate declaration
+        private delegate void delVoidInt(int i);
+
+        // Constants
+        private const int WIDTH = 800;
+        private const int HEIGHT = 600;
+        private const int BLOCKSIZE = 10;
+        private const int X_MAX = WIDTH / BLOCKSIZE;
+        private const int Y_MAX = HEIGHT / BLOCKSIZE;
+        private const int BASE_RGB_VALUE = 50;
+        
+        public static Random _rnd = new Random();       // Random number generator
+        private Thread _thread = null;                  // Active thread checker
+        
+
+        public BlockFill()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_BTN_Start_Click(object sender, EventArgs e)
+        {
+            // Program is still running
+            if (_thread != null)
+                return;
+            if (UI_RB_Unbiased.Checked)
+            {
+                // Start a new thread to fill the block
+                _thread = new Thread(TraverseGridRandomly);
+                _thread.IsBackground = true;
+                _thread.Start();
+            }
+        }
+
+        // Thread to traverse 2D grid
+        private void TraverseGridRandomly()
+        {
+            int steps = 0;
+            CDrawer canvas = new CDrawer(WIDTH, HEIGHT);
+            int[,] grid = new int[X_MAX, Y_MAX];    // 2D backing array for the grid  
+            Point current = new Point(0, 0);
+            Point previous = new Point(0, 0);
+
+            List<Point> nextPossiblePoint = new List<Point>();
+
+            canvas.Scale = BLOCKSIZE;
+            // Initialize each grid location with nominal value;
+            for (int x = 0; x < X_MAX; ++x)
+                for (int y = 0; y < Y_MAX; ++y)
+                    grid[x, y] = BASE_RGB_VALUE;
+
+            while (!HasTravelledAll(grid))
+            {
+                ++steps;
+
+                // Increment intensity of current color to max of 255
+                grid[current.X, current.Y] = 
+                    grid[current.X, current.Y] + 3 > 255 ? 255 : grid[current.X, current.Y] + 3;
+
+                if (steps != 1)
+                    canvas.SetBBScaledPixel(previous.X, previous.Y,
+                        Color.FromArgb(grid[previous.X, previous.Y], grid[previous.X, previous.Y], 0));
+
+                canvas.SetBBScaledPixel(current.X, current.Y, Color.Red);
+
+                previous = current;
+
+                nextPossiblePoint.Clear();
+                nextPossiblePoint = validLocation(current);
+
+                current = nextPossiblePoint[_rnd.Next(0, nextPossiblePoint.Count)];
+            }
+
+            canvas.AddText(steps.ToString() + " Steps Taken", 25, Color.Blue);
+            try
+            {
+                Invoke(new delVoidInt(UpdateUI), steps);
+            }
+            catch
+            {
+                System.Diagnostics.Trace.WriteLine("Target thread is dead");
+            }
+            _thread = null;
+        }
+
+        private List<Point> validLocation(Point location)
+        {
+            List<Point> validPoints = new List<Point>();
+            
+            // Add points left, right, up, and down
+            validPoints.Add(new Point(location.X - 1, location.Y));
+            validPoints.Add(new Point(location.X + 1, location.Y));
+            validPoints.Add(new Point(location.X, location.Y - 1));
+            validPoints.Add(new Point(location.X, location.Y + 1));
+
+            // remove all points out of bounds
+            validPoints.RemoveAll(IsOutOfBounds);
+
+            return validPoints;
+        }
+
+        // Check if the point is still with in the grid;
+        private bool IsOutOfBounds(Point location)
+        {
+            if (location.X < 0 || location.X >= X_MAX || location.Y < 0 || location.Y >= Y_MAX)
+                return true;
+            return false;
+        }
+
+        // Checks if all cells have been travelled to
+        private bool HasTravelledAll(int[,] array2D)
+        {
+            foreach (int i in array2D)
+                if (i == BASE_RGB_VALUE)
+                    return false;
+            
+            return true;
+        }
+
+        // Callback function for delegate
+        private void UpdateUI(int count)
+        {
+            UI_TB_Steps.Text = count.ToString();
+        }
+    }
+}
